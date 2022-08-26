54933043c62f30c0cf35e6ad4cdaba4a57f641ca
Franco9400 <109389263+Franco9400@users.noreply.github.com>
2022-08-25 21:09:18 -0300

    commit de modificacion del bash creacion de contenedor

--- reto1/contenedor.sh
+++ reto1/contenedor.sh
@@ -1,11 +1,17 @@
 
+#Autor : Franco López
 
+#Script en bash para crear un contenedor
 
-docker run -d --name servidor_web -p 8181:80 nginx
+input_type=""
+input_text=""
+read -p "Desea proceder con la creacion del contenedor (y) or (n) ?" input_type
+if (( $input_type == y )); then
+	docker run -d --name servidor_web -p 8181:80 nginx
+	docker ps
+	docker images
+	docker ps -a
+	read -p "El contenedor ha sido creado con el nombre servidor_web y se encuentra corriendo"
 
-docker ps
-
-docker images
-
-docker ps -a
+fi
  
